2ff9e7adff3722564d7e00dbb3410c1e814fbf1e
Daniil Pechinkin <pomor44@gmail.com>
2024-05-14 21:46:54 +0300

    add .gitignore & initial cmake

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+using ill = int long long;
+using vi = vector<int long long>;
+
+
+void solve() {
+
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
